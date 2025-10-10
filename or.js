8f49550d161f8ec4edea5391327bd37672db921f
devyanis05 <shelkegaju700@gmail.com>
2025-10-10 15:11:39 +0530

    or

---
+++ or.js
@@ -0,0 +1,8 @@
+var students = 40;
+var buses = 2 ;
+if (students > 30 || buses < 2) {
+    console.log("We have enough buses , the trip is on");
+}   
+else {
+    console.log("We don't have enough buses , the trip is cancelled");
+}
