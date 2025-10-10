79db9f1111240b6e8758b7e9f3a04972b89c89c9
devyanis05 <shelkegaju700@gmail.com>
2025-10-10 22:25:56 +0530

    method

---
+++ methods.js
@@ -0,0 +1,16 @@
+var text = "Devyani" ;
+
+console.log(text.length);   //String Length
+
+console.log(text.charAt(4));  //String CharAT  
+
+console.log(text.charCodeAt(4));  //String charCodeAt
+
+console.log(text.codePointAt(5));  //String codePointAt
+
+console.log(text.at(3));  //String at
+
+console.log(text[4]);  //String Indexing
+
+console.log(text.slice(0,3));  //String Slice
+
