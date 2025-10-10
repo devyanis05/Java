e7abb340d33d36f9480bf7b42202bb9477a66422
devyanis05 <shelkegaju700@gmail.com>
2025-10-10 15:02:12 +0530

    and

---
+++ and.js
@@ -0,0 +1,8 @@
+const marks = 50;
+const attendance = 75;
+if (marks >= 45 && attendance >= 75) {
+    console.log("You are appear for the exam");
+} 
+else {
+    console.log("You are not appear for the exam");
+}
