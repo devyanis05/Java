f45c5771cda7dbf468742f151148f6a82828d1b8
devyanis05 <shelkegaju700@gmail.com>
2025-10-22 18:59:01 +0530

    function

---
+++ Object/function.js
@@ -0,0 +1,8 @@
+function Person(first, last, age, eye) {
+  this.firstName = first;
+  this.lastName = last;
+  this.age = age;
+  this.eyeColor = eye;
+}
+const mySelf = new Person("Devyani", "Shelke", 20, "brown");
+console.log(mySelf.lastName);
