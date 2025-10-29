5eecd185ad346e7d15ed89ea42b05694a046fe2d
devyanis05 <shelkegaju700@gmail.com>
2025-10-29 14:46:42 +0530

    userr input

---
+++ Array/prompt.js
@@ -0,0 +1,7 @@
+const flowers = []; 
+flowers[0] = prompt("Enter the name of flower 1:");
+flowers[1] = prompt("Enter the name of flower 2:");
+flowers[2] = prompt("Enter the name of flower 3:");
+flowers[3] = prompt("Enter the name of flower 4:");
+flowers[4] = prompt("Enter the name of flower 5:");
+document.write(flowers[0]);
