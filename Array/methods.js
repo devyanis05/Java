67b28ed1d9a98284808e6ba1f062da9a77396c9a
devyanis05 <shelkegaju700@gmail.com>
2025-10-30 22:52:55 +0530

    some more methods added

--- Array/methods.js
+++ Array/methods.js
@@ -1,4 +1,36 @@
 const fruits = ["Apple", "Banana", "Cherry", "Mango", "Orange"];
 
 let a = fruits.length;// length property
-document.write(a); 
+document.write(a);
+
+document.write("<br>");
+
+let b = fruits.toString(); // toString() method
+document.write(b);
+
+document.write("<br>");
+
+let c = fruits.join(" - "); // join() method
+document.write(c);
+
+document.write("<br>");
+
+fruits.pop(); // pop() method
+document.write(fruits);
+
+document.write("<br>");
+fruits.push("Pineapple"); // push() method
+document.write(fruits);
+
+document.write("<br>");
+fruits.shift(); // shift() method
+document.write(fruits);
+
+document.write("<br>");
+fruits.unshift("Strawberry"); // unshift() method
+document.write(fruits);
+
+document.write("<br>");
+delete fruits [0] // delete method
+document.write(fruits);
+
